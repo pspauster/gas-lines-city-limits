855ecf9494700813cdda97ea4ce548c6fe7cd3cc
Patrick Spauster <patrick.spauster@gmail.com>
2025-02-24 20:49:37 -0500

    the geometries i drew are being really dumb

--- script.R
+++ script.R
@@ -3,12 +3,16 @@ library(sf)
 library(mapview)
 
 lines <- read_sf("data/map-2.geojson") %>% 
-  st_make_valid()
+  st_make_valid() %>% 
+  mutate(geo_type = "line")
 
 st_crs(lines)
 mapview(lines)
 
-points <- read_sf("data/points-layer.geojson")
+points <- read_sf("data/points-layer.geojson") %>% 
+  st_make_valid() %>% 
+  mutate(geo_type = "point",
+         Proposed = "Yes")
 st_crs(points)
 mapview(points)
 
@@ -18,3 +22,9 @@ ggplot(points)+
 
 m <- mapview(lines)
 leafem::addFeatures(m, points)
+
+all_together <- bind_rows(lines, points)
+
+st_write(all_together, "data/gas_infra.geojson", append = F)
+all_together %>% filter(Proposed == "Yes", geo_type == "line") %>% 
+  st_write("data/test.geojson")
